e90757f6cf00dbab942756ec48ed5f4389fc6e4a
szatolokin <staszatolokin28@gmail.com>
2021-05-25 13:05:20 +0300

    Start header modals functionality

--- src/app.js
+++ src/app.js
@@ -1,1 +1,47 @@
 import 'Styles/_app.scss';
+
+// header
+{
+  $(() => {
+    const header = $('.header');
+
+    if (header.length !== 0) {
+      // modal
+      {
+        const state = {
+          id: null,
+          close: function () {
+            $(`[data-modal-id="${this.id}"]`).removeAttr('data-modal-active');
+            $(`[data-modal-button="${this.id}"]`).removeAttr('data-modal-active');
+          },
+          open: function () {
+            $(`[data-modal-id="${this.id}"]`).attr('data-modal-active', '');
+            $(`[data-modal-button="${this.id}"]`).attr('data-modal-active', '');
+          },
+          update: function (id) {
+            this.id = id;
+          },
+          change: function (id) {
+            this.close();
+            this.update(id === this.id ? null : id);
+            this.open();
+          }
+        };
+
+        const btn = $('[data-modal-button]');
+        btn.on('click', function () {
+          const id = $(this).data('modal-button');
+          state.change(id);
+        });
+
+        $(window).on('click', event => {
+          const isHeader = $(event.target).closest(header).length !== 0;
+
+          if (!isHeader) {
+            state.change(null);
+          }
+        });
+      }
+    }
+  });
+}
