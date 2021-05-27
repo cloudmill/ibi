8906d1adfb322b11f1e93bf626d7d5f76c1bfd46
szatolokin <staszatolokin28@gmail.com>
2021-05-27 12:55:07 +0300

    Update about page
    Add slider js, add slides to about page

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import 'Styles/_app.scss';
+import Swiper from 'swiper';
 
 const BREAKPOINT = 1280;
 
@@ -131,4 +132,53 @@ const BREAKPOINT = 1280;
       });
     }
   });
+}
+
+// slider
+{
+  const slider = $('[data-slider-id]');
+
+  if (slider.length !== 0) {
+    slider.each(function () {
+      const slider_el = $(this);
+      const slider_id = slider_el.data('slider-id');
+      const slider_prev_id = slider_el.data('slider-prev');
+      const slider_next_id = slider_el.data('slider-next');
+      const slider_prev = $(`[data-slider-button="${slider_prev_id}"]`);
+      const slider_next = $(`[data-slider-button="${slider_next_id}"]`);
+      
+      let slider_options = {
+        slidesPerView: 'auto',
+
+        spaceBetween: 20,
+
+        breakpoints: {
+          [BREAKPOINT]: {
+            spaceBetween: 40,
+          },
+        },
+      };
+
+      // example
+      // switch (slider_id) {
+      //   case 1:
+      //   case 2:
+      //     slider_options = {
+      //       ...slider_options,
+
+      //       spaceBetween: 80,
+      //     }
+      //     break;
+      // }
+
+      const slider_swiper = new Swiper(slider_el[0], slider_options);
+
+      slider_prev.on('click', () => {
+        slider_swiper.slidePrev();
+      });
+      slider_next.on('click', () => {
+        slider_swiper.slideNext();
+      });
+    });
+  }
 }
