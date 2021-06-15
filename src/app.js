dca4107d783e33ff9e02b0cb5ae1ce136cd55a02
Kirill Matveev <iamdejar@gmail.com>
2021-06-15 17:58:36 +0300

    Added validation to the form on the doctor's page, made a mask in the phone input field, added date attributes to the filter

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import { each } from 'jquery';
 import 'Styles/_app.scss';
 import Swiper from 'swiper';
+import Parsley from 'parsleyjs';
 
 const BREAKPOINT = 1280;
 
@@ -278,4 +279,106 @@ const BREAKPOINT = 1280;
       });
     });
   });
+}
+
+// team filter
+
+{
+  // let memberCard = document.querySelectorAll('.team-member');
+  
+  // document.querySelector('.team-filter').addEventListener('click', event => {
+  //   if (event.target.tagName != 'LI') return false;
+
+  //   let filterClass = event.target.dataset['id'];
+  //   // console.log(filterClass);
+
+  //   memberCard.forEach(elem => {
+  //     elem.classList.remove('hide');
+  //     if (!elem.classList.contains(filterClass)) {
+  //       elem.classList.add('hide'); 
+  //     }
+  //   });
+  // });
+
+
+}
+
+// tel mask
+
+{
+  document.addEventListener("DOMContentLoaded", function () {
+    var phoneInputs = document.querySelectorAll('input[data-tel-input]');
+    console.log(phoneInputs);
+
+    var getInputNumbersValue = function (input) {
+        // Удаление любых символов крме цифр
+        return input.value.replace(/\D/g, '');
+    }
+
+    // Очистка скопированного и вставленного в поле номера
+    var onPhonePaste = function (e) {
+        var input = e.target,
+            inputNumbersValue = getInputNumbersValue(input);
+        var pasted = e.clipboardData || window.clipboardData;
+        if (pasted) {
+            var pastedText = pasted.getData('Text');
+            if (/\D/g.test(pastedText)) {
+                input.value = inputNumbersValue;
+                return;
+            }
+        }
+    }
+
+    // Обработка вписанного вручную номера
+    var onPhoneInput = function (e) {
+        var input = e.target,
+            inputNumbersValue = getInputNumbersValue(input),
+            selectionStart = input.selectionStart,
+            formattedInputValue = "";
+
+        if (!inputNumbersValue) {
+            return input.value = "";
+        }
+
+        if (input.value.length != selectionStart) {
+            if (e.data && /\D/g.test(e.data)) {
+                input.value = inputNumbersValue;
+            }
+            return;
+        }
+
+        if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+            if (inputNumbersValue[0] == "9") inputNumbersValue = "7" + inputNumbersValue;
+            var firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7";
+            formattedInputValue = input.value = firstSymbols + " ";
+            if (inputNumbersValue.length > 1) {
+                formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
+            }
+            if (inputNumbersValue.length >= 5) {
+                formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
+            }
+            if (inputNumbersValue.length >= 8) {
+                formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+            }
+            if (inputNumbersValue.length >= 10) {
+                formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+            }
+        } else {
+            formattedInputValue = '+' + inputNumbersValue.substring(0, 16);
+        }
+        input.value = formattedInputValue;
+    }
+    var onPhoneKeyDown = function (e) {
+        // Удаление первого символа после удаления номера
+        var inputValue = e.target.value.replace(/\D/g, '');
+        if (e.keyCode == 8 && inputValue.length == 1) {
+            e.target.value = "";
+        }
+    }
+    for (var phoneInput of phoneInputs) {
+        phoneInput.addEventListener('keydown', onPhoneKeyDown);
+        phoneInput.addEventListener('input', onPhoneInput, false);
+        phoneInput.addEventListener('paste', onPhonePaste, false);
+    }
+})
 }
