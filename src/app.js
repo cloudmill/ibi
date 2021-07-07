b12bbfd37f810cdfe30b135cf03c8b7609173abc
Nikita <nikita.ableev@mail.ru>
2021-07-07 11:33:11 +0300

    Update implantation slider

--- src/app.js
+++ src/app.js
@@ -704,3 +704,20 @@ const BREAKPOINT = 1280;
     $('.beer-slider').BeerSlider({start: 35});
   });
 }
+
+// slide description
+{
+  $(() => {
+    if ($('.index').length !== 0) {
+      const indexSlide = $('.swiper-slide');
+      indexSlide.each(function() {
+        const slide = $(this);
+        const slideImg = slide.find('.index__mid-img');
+        const slideDesc = slide.find('.index__mid-description');
+        const imgWidth = slideImg.width();
+
+        slideDesc.css('max-width', imgWidth);
+      });
+    }
+  });
+}
