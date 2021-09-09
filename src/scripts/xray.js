2f0064a4ebc5476e61250ce591e7d3a9b2db5851
szatolokin <staszatolokin28@gmail.com>
2021-09-09 17:14:01 +0300

    Update xray

--- src/scripts/xray.js
+++ src/scripts/xray.js
@@ -1,86 +1,128 @@
-const IMAGE_WIDTH = 1920
-const IMAGE_HEIGHT = 915
+let IMAGE_WIDTH;
+let IMAGE_HEIGHT;
 
-const ELLIPSE_WIDTH = 383
-const ELLIPSE_HEIGHT = 230
+const ELLIPSE_WIDTH = 383;
+const ELLIPSE_HEIGHT = 230;
+const ELLIPSE_ANGLE = 15;
 
-const MOUSEMOVE_FPS = 60
+const MOUSEMOVE_FPS = 60;
 
 $(() => {
-  const components = $('.xray')
+  const components = $(".xray");
 
   components.each(function () {
-    const component = $(this)
-    const ellipse = component.find('.xray__ellipse')
-    const images = component.find('.xray__image')
+    const component = $(this);
 
-    function initComponent() {
-      component.attr('viewBox', `0 0 ${IMAGE_WIDTH} ${IMAGE_HEIGHT}`)
+    const images = component.find(".xray__image");
+    const ellipse = component.find(".xray__ellipse");
 
-      ellipse.attr('cx', -(ELLIPSE_WIDTH / 2))
-      ellipse.attr('cy', -(ELLIPSE_HEIGHT / 2))
-      ellipse.attr('rx', ELLIPSE_WIDTH / 2)
-      ellipse.attr('ry', ELLIPSE_HEIGHT / 2)
+    function getImageSize() {
+      let minImageWidth = null;
+      let minImageHeight = null;
 
-      images.attr('width', IMAGE_WIDTH)
-      images.attr('height', IMAGE_HEIGHT)
+      images.each(function () {
+        const imageStyle = getComputedStyle(this);
+
+        const imageWidth = +imageStyle.width.slice(0, -2);
+        const imageHeight = +imageStyle.height.slice(0, -2);
+
+        minImageWidth =
+          minImageWidth === null
+            ? imageWidth
+            : imageWidth < minImageWidth
+            ? imageWidth
+            : minImageWidth;
+
+        minImageHeight =
+          minImageHeight === null
+            ? imageHeight
+            : imageHeight < minImageHeight
+            ? imageHeight
+            : minImageHeight;
+      });
+
+      return [minImageWidth, minImageHeight];
     }
 
-    initComponent()
+    [IMAGE_WIDTH, IMAGE_HEIGHT] = getImageSize();
+
+    console.log(IMAGE_WIDTH, IMAGE_HEIGHT)
+
+    function initComponent() {
+      component.attr("viewBox", `0 0 ${IMAGE_WIDTH} ${IMAGE_HEIGHT}`);
+
+      ellipse.attr("cx", -(ELLIPSE_WIDTH / 2));
+      ellipse.attr("cy", -(ELLIPSE_HEIGHT / 2));
+      ellipse.attr("rx", ELLIPSE_WIDTH / 2);
+      ellipse.attr("ry", ELLIPSE_HEIGHT / 2);
+
+      images.attr("width", IMAGE_WIDTH);
+      images.attr("height", IMAGE_HEIGHT);
+    }
+
+    initComponent();
 
     function updateEllipse(event) {
-      const mouseX = event.originalEvent.clientX
-      const mouseY = event.originalEvent.clientY
+      const mouseX = event.originalEvent.clientX;
+      const mouseY = event.originalEvent.clientY;
 
-      const componentPos = component[0].getBoundingClientRect()
+      const componentPos = component[0].getBoundingClientRect();
 
-      const componentX = componentPos.x
-      const componentY = componentPos.y
-      const componentWidth = componentPos.width
-      const componentHeight = componentPos.height
+      const componentX = componentPos.x;
+      const componentY = componentPos.y;
+      const componentWidth = componentPos.width;
+      const componentHeight = componentPos.height;
 
-      const componentRatio = componentWidth / componentHeight
-      const imageRatio = IMAGE_WIDTH / IMAGE_HEIGHT
+      const componentRatio = componentWidth / componentHeight;
+      const imageRatio = IMAGE_WIDTH / IMAGE_HEIGHT;
 
       if (componentRatio < imageRatio) {
-        const imageWidth = imageRatio * componentHeight
+        const imageWidth = imageRatio * componentHeight;
 
-        const x = mouseX - (componentX - ((imageWidth - componentWidth) / 2))
-        const y = mouseY - componentY
+        const x = mouseX - (componentX - (imageWidth - componentWidth) / 2);
+        const y = mouseY - componentY;
 
-        const xNorm = x / imageWidth
-        const yNorm = y / componentHeight
+        const xNorm = x / imageWidth;
+        const yNorm = y / componentHeight;
 
-        const ellipseX = IMAGE_WIDTH * xNorm
-        const ellipseY = IMAGE_HEIGHT * yNorm
-        
-        ellipse.attr('cx', ellipseX)
-        ellipse.attr('cy', ellipseY)
+        const ellipseX = IMAGE_WIDTH * xNorm;
+        const ellipseY = IMAGE_HEIGHT * yNorm;
+
+        ellipse.attr("cx", ellipseX);
+        ellipse.attr("cy", ellipseY);
+        ellipse.attr(
+          "transform",
+          `rotate(${ELLIPSE_ANGLE} ${ellipseX} ${ellipseY})`
+        );
       } else {
-        const imageHeight = componentWidth / imageRatio
+        const imageHeight = componentWidth / imageRatio;
 
-        const x = mouseX - componentX
-        const y = mouseY - (componentY - ((imageHeight - componentHeight) / 2))
+        const x = mouseX - componentX;
+        const y = mouseY - (componentY - (imageHeight - componentHeight) / 2);
 
-        const xNorm = x / componentWidth
-        const yNorm = y / imageHeight
+        const xNorm = x / componentWidth;
+        const yNorm = y / imageHeight;
 
-        const ellipseX = IMAGE_WIDTH * xNorm
-        const ellipseY = IMAGE_HEIGHT * yNorm
+        const ellipseX = IMAGE_WIDTH * xNorm;
+        const ellipseY = IMAGE_HEIGHT * yNorm;
 
-        ellipse.attr('cx', ellipseX)
-        ellipse.attr('cy', ellipseY)
+        ellipse.attr("cx", ellipseX);
+        ellipse.attr("cy", ellipseY);
+        ellipse.attr(
+          "transform",
+          `rotate(${ELLIPSE_ANGLE} ${ellipseX} ${ellipseY})`
+        );
       }
     }
 
     function handleMousemove(event) {
-      updateEllipse(event)
+      updateEllipse(event);
 
       setTimeout(() => {
-        $(window).one('mousemove', handleMousemove)
-      }, 1000 / MOUSEMOVE_FPS)
+        $(window).one("mousemove", handleMousemove);
+      }, 1000 / MOUSEMOVE_FPS);
     }
 
-    $(window).one('mousemove', handleMousemove)
-  })
-})
+    $(window).one("mousemove", handleMousemove);
+  });
+});
