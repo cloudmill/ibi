98090469c277df9d5faabc853395d6220b84acb2
Kirill Matveev <dejar@protonmail.com>
2021-09-15 19:23:30 +0300

    Worked on animation of inputs

--- src/scripts/forms.js
+++ src/scripts/forms.js
@@ -3,6 +3,8 @@
 //   $(() => {
 //     const form = $('.form');
 
+import { event } from "jquery";
+
 //     if (form.length !== 0) {
 
 //       const response = $('.response');
@@ -177,71 +179,42 @@
       }
     }
   })
-
-  
-  const secFunction = function( window ) {
-    
-    // class helper functions from bonzo https://github.com/ded/bonzo
-    
-    function classReg( className ) {
-      return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-    }
-    
-    // classList support for class management
-    // altho to be fair, the api sucks because it won't accept multiple classes at once
-    var hasClass, addClass, removeClass;
-    
-    if ( 'classList' in document.documentElement ) {
-      hasClass = function( elem, c ) {
-        return elem.classList.contains( c );
-      };
-      addClass = function( elem, c ) {
-        elem.classList.add( c );
-      };
-      removeClass = function( elem, c ) {
-        elem.classList.remove( c );
-      };
-    }
-    else {
-      hasClass = function( elem, c ) {
-        return classReg( c ).test( elem.className );
-      };
-      addClass = function( elem, c ) {
-        if ( !hasClass( elem, c ) ) {
-          elem.className = elem.className + ' ' + c;
-        }
-      };
-      removeClass = function( elem, c ) {
-        elem.className = elem.className.replace( classReg( c ), ' ' );
-      };
-    }
-    
-    function toggleClass( elem, c ) {
-      var fn = hasClass( elem, c ) ? removeClass : addClass;
-      fn( elem, c );
-    }
-    
-    var classie = {
-      // full names
-      hasClass: hasClass,
-      addClass: addClass,
-      removeClass: removeClass,
-      toggleClass: toggleClass,
-      // short names
-      has: hasClass,
-      add: addClass,
-      remove: removeClass,
-      toggle: toggleClass
-    };
-    
-    // transport
-    if ( typeof define === 'function' && define.amd ) {
-      // AMD
-      define( classie );
-    } else {
-      // browser global
-      window.classie = classie;
-    }
-    
-  }
 }
+
+{
+  const input = $('.news-subscribe__form-email');
+  const label = $('.news-subscribe__form-placeholder');
+
+  $(() => {
+    input.on('click', function(event) {
+      const currentInput = $(event.target).closest('.news-subscribe__form-placeholder');
+      currentInput.addClass('input-focus');
+    })
+  })
+
+  $(window).on('click', event => {
+    if (
+      $(event.target).closest(input).length === 0
+    ) {
+      label.removeClass('input-focus')
+    } else {
+      label.removeClass('input-focus');
+      $(event.target).closest(label).addClass('input-focus');
+    }
+  })
+
+  // console.log(
+  //   input.validate()
+  // );
+}
+
+// {
+//   $('.news-subscribe__form-btn').on('click', () => {
+ 
+//     setTimeout(() => $('.parsley-error').closest('.news-subscribe__form-placeholder').addClass('form-error'));
+//     // console.log($('.parsley-error'));
+    
+//   })
+
+
+// }
