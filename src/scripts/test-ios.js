d48d41e39943b22c8ea1fde7ad0a580e3a7b3388
szatolokin <staszatolokin28@gmail.com>
2021-11-03 16:18:20 +0300

    # Update seq test page

--- src/scripts/test-ios.js
+++ src/scripts/test-ios.js
@@ -1,26 +1,24 @@
-console.log('test-ios')
-
-function getHeight(element) {
-  try {
-    const elementRect = element.getBoundingClientRect()
-    return elementRect.height
-  } catch (err) {
-    console.error(err)
-    return 0
-  }
-}
-
-function setConsole(...msgs) {
-  try {
-    const consoleEl = document.querySelector('.test-ios-console')
-    consoleEl.innerHTML = msgs.map(msg => `${msg}<br>`).join('')
-  } catch (err) {
-    console.log(err)
-  }
-}
-
 window.addEventListener('DOMContentLoaded', () => {
   if (document.querySelector('.test-ios')) {
+    function getHeight(element) {
+      try {
+        const elementRect = element.getBoundingClientRect()
+        return elementRect.height
+      } catch (err) {
+        console.error(err)
+        return 0
+      }
+    }
+    
+    function setConsole(...msgs) {
+      try {
+        const consoleEl = document.querySelector('.test-ios-console')
+        consoleEl.innerHTML = msgs.map(msg => `${msg}<br>`).join('')
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
     setInterval(() => {
       setConsole(
         `inset: 0 - ${getHeight(document.querySelector('.test-ios-1'))}`,
