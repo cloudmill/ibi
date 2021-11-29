e5106c1936a44ae517f60852493358c6d04edf6d
Kirill Matveev <dejar@protonmail.com>
2021-11-29 14:05:20 +0300

    Добавил ссылки в сайдбар на странице протезирования

--- src/scripts/sidebar-spoiler.js
+++ src/scripts/sidebar-spoiler.js
@@ -1,35 +1,49 @@
 {
   $(() => {
     const navSpoilerList = $('.nav__list');
-    const navSpoiler = navSpoilerList.find('.sidebar-nav-spoiler');
+    // const navSpoiler = navSpoilerList.find('.sidebar-nav-spoiler');
     const navSpoilerBtn = navSpoilerList.find('.sidebar-nav-spoiler__button');
-    const navSpoilerDrop = navSpoilerList.find('.sidebar-nav-spoiler__drop');
+    // const navSpoilerDrop = navSpoilerList.find('.sidebar-nav-spoiler__drop');
+    const spoilerArrow = navSpoilerList.find('.sidebar-nav-spoiler__arrow');
+
+    console.log(spoilerArrow);
 
     // console.log(() => {navSpoilerList.getBoundingClientRect()});
 
-    navSpoilerBtn.on('click', event => {
-      let currentSpoiler = $(event.target).closest('.sidebar-nav-spoiler');
-      let currentSpoilerDrop = currentSpoiler.find('.sidebar-nav-spoiler__drop');
+    if (spoilerArrow.length !== 0) {
+      
+      console.log('halo1');
 
-      // console.log(currentSpoiler);
+      spoilerArrow.on('click', event => {
 
-      if (currentSpoiler.hasClass('sidebar-nav-spoiler--active')) {
+        console.log('halo2');
 
-        // console.log(111);
-
-        currentSpoiler.find('.sidebar-nav-spoiler__drop').slideUp(500);
-        currentSpoiler.removeClass('sidebar-nav-spoiler--active');
-      } else {
-
-        // console.log(222);
-
-        $('.sidebar-nav-spoiler--active').find('.sidebar-nav-spoiler__drop').slideUp(500);
-        $('.sidebar-nav-spoiler--active').removeClass('sidebar-nav-spoiler--active');
-
-        currentSpoilerDrop.slideDown(500);
-        currentSpoiler.addClass('sidebar-nav-spoiler--active');
-      }
-    })
+        let currentSpoiler = $(event.target).closest('.sidebar-nav-spoiler');
+        let currentSpoilerDrop = currentSpoiler.find('.sidebar-nav-spoiler__drop');
+  
+        // currentSpoilerDrop.slideToggle(500);
+        // currentSpoiler.toggleClass('sidebar-nav-spoiler--active');
+  
+        // console.log(currentSpoiler);
+  
+        if (currentSpoiler.hasClass('sidebar-nav-spoiler--active')) {
+  
+          // console.log(111);
+  
+          currentSpoiler.find('.sidebar-nav-spoiler__drop').slideUp(500);
+          currentSpoiler.removeClass('sidebar-nav-spoiler--active');
+        } else {
+  
+          // console.log(222);
+  
+          $('.sidebar-nav-spoiler--active').find('.sidebar-nav-spoiler__drop').slideUp(500);
+          $('.sidebar-nav-spoiler--active').removeClass('sidebar-nav-spoiler--active');
+  
+          currentSpoilerDrop.slideDown(500);
+          currentSpoiler.addClass('sidebar-nav-spoiler--active');
+        }
+      })
+    }
   })
 }
 
