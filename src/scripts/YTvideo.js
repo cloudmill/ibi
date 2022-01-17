d9021ed159f928e72c47dce06c096663a19ba446
Kirill Matveev <dejar@protonmail.com>
2022-01-17 12:24:56 +0300

    Fixed video

--- src/scripts/YTvideo.js
+++ src/scripts/YTvideo.js
@@ -1,20 +1,29 @@
 $(window).on('load', () => {
-  var player;
-  function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-      videoId: 'O6zUpjtU6hE',
-      
-    });
-  }
 
-  onYouTubeIframeAPIReady();
+  const video = document.querySelector('[data-video]');
 
-  const video = document.querySelector('[data-video]')
-  const button = video.querySelector('[data-video-button]')
-  const img = video.querySelector('.video-preview__img')
+  if (video.length !== 0) {
+    let videoId = $('#player').attr('data-video-id');
 
-  button.onclick = function() {
-    player.playVideo()
-    img.classList.add('video-preview__img--hide')
+    console.log(videoId);
+
+    var player;
+    function onYouTubeIframeAPIReady() {
+      player = new YT.Player('player', {
+        videoId: videoId,
+        
+      });
+    }
+  
+    onYouTubeIframeAPIReady();
+  
+    const video = document.querySelector('[data-video]')
+    const button = video.querySelector('[data-video-button]')
+    const img = video.querySelector('.video-preview__img')
+  
+    button.onclick = function() {
+      player.playVideo()
+      img.classList.add('video-preview__img--hide')
+    }
   }
 })
