cca6fca35c9eeb9840b9b45df98958bb83da24d8
Kirill Matveev <dejar@protonmail.com>
2022-02-10 15:01:11 +0300

    Upd, build

--- src/scripts/YTvideo.js
+++ src/scripts/YTvideo.js
@@ -10,17 +10,15 @@ $(window).on('load', () => {
     function onYouTubeIframeAPIReady() {
       player = new YT.Player('player', {
         videoId: videoId,
-        
       });
     }
   
     onYouTubeIframeAPIReady();
   
-    const video = document.querySelector('[data-video]')
     const button = video.querySelector('[data-video-button]')
     const img = video.querySelector('.video-preview__img')
   
-    button.onclick = function() {
+    img.onclick = function() {
       player.playVideo()
       img.classList.add('video-preview__img--hide')
     }
