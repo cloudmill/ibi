0af8b5187d300477cdb2f7020ba7b81afc7e29fb
kirill matveev <dejar@protonmail.com>
2022-02-17 15:51:21 +0300

    Fixed implant tabs

--- src/scripts/tab-nav.js
+++ src/scripts/tab-nav.js
@@ -1,8 +1,26 @@
-window.addEventListener('load', () => {
-  if ($('.tab-nav').length !== 0) {
+// window.addEventListener('DOMContentLoaded', () => {
+//   if ($('.tab-nav').length !== 0) {
+
   
-  // скролл эл-та .tab-nav к активному эл-ту
-  $('.tab-nav').scrollLeft(
-    $('.category__item--active').offset().left - 20
-    )}
-});
+//     if ($('.implantation2').length) {
+//       $('.tab-nav').scrollLeft(150);
+//       console.log('yooo');
+//     }
+
+//     if ($('.implantation3').length) {
+//       $('.tab-nav').scrollLeft(390);
+//       console.log('yoo');
+//     }
+
+//     if ($('.implantation4').length) {
+//       $('.tab-nav').scrollLeft(551);
+//       console.log('yo');
+//     }
+
+//     if ($('.implantation5').length) {
+//       $('.tab-nav').scrollLeft(390);
+//       console.log('yooooooooo');
+//     }
+  
+//   }
+// });
